b716ca2cf699a4633d9ac821c6cf2fa7a79c0b2b
hilmanadi <hilmanadi1995@gmail.com>
2020-03-31 19:42:58 +0700

    update export to excel

--- checkplagiasi.py
+++ checkplagiasi.py
@@ -1,20 +1,28 @@
 import json
 import os
+import xlsxwriter
+
+row = 1
+col = 0	
 
 arr = []
-newarr = []
+arranyar =[]
+
+pointerr =''
 
 def c_file():
 	fl = os.listdir('.')
 
 	for i in fl:
-		if i != 'checkplagiasi.py':
+		if i != 'checkplagiasi.py' and i != '.git' !='.xlsx':
 			arr.append(i)
-
+	la = len(arr)
+	
 	for x in arr:
-		get_file_data(x)
+		get_file_data(x,la)
 
-def get_file_data(a):
+def get_file_data(a,b):
+	
 	substringfor='for'
 	substringif='if'
 	substringelse='else'
@@ -69,6 +77,10 @@ def get_file_data(a):
 			print 'elif',findelif
 			print 'def',finddef
 			print 'import',findimport
+			global pointerr
+			pointerr = 'python'
+			cobadict = {'nama_file':a,substringif:findif,substringfor:findfor,substringelse:findelse,substringkurawal1:findkurawal1,substringkurawal2:findkurawal2,substringarr1:findarr1,substringarr2:findarr2,'singlequote':findsinglequote,'doublequote':finddoublequote,substringjson:findjson,substringclass:findclass,substringreturn:findreturn,substringdef:finddef,substringelif:findelif,substringimport:findimport}
+			arranyar.append(cobadict)
 
 		elif '.java' in a:
 			substringimport='import'
@@ -111,13 +123,61 @@ def get_file_data(a):
 		else:
 			print 'lainnya'
 def proc():
-	for i in newarr:
-		if i[1]>=9:
-			print '>= 9',i[0]
-			# print 'a'
-		else:
-			print '<9',i[0]
-	
+	if pointerr == 'python':
+		pyexcel()
+	elif pointerr =='java':
+		javaexcel()
+	else:
+		print 'lainnya'
+
+
+def pyexcel():
+	wb = xlsxwriter.Workbook('python.xlsx')
+
+	wst = wb.add_worksheet()
+
+	wst.write('A1','Nama File')
+	wst.write('B1','Jumlah If')
+	wst.write('C1','Jumlah Else')
+	wst.write('D1','Jumlah Elif')
+	wst.write('E1','Jumlah For')
+	wst.write('F1','Jumlah {')
+	wst.write('G1','Jumlah }')
+	wst.write('H1','Jumlah [')
+	wst.write('I1','Jumlah ]')
+	wst.write('J1','Jumlah Singlequote')
+	wst.write('K1','Jumlah Doublequote')
+	wst.write('L1','Jumlah JSON')
+	wst.write('M1','Jumlah Class')
+	wst.write('N1','Jumlah Return')
+	wst.write('O1','Jumlah Def')
+	wst.write('P1','Jumlah Import')
+
+	for i in range(len(arranyar)):
+		wst.write(row, col, arranyar[i]['nama_file']) 
+		wst.write(row, col + 1, arranyar[i]['if']) 
+		wst.write(row, col + 2, arranyar[i]['else']) 
+		wst.write(row, col + 3, arranyar[i]['elif']) 
+		wst.write(row, col + 4, arranyar[i]['for']) 
+		wst.write(row, col + 5, arranyar[i]['{']) 
+		wst.write(row, col + 6, arranyar[i]['}']) 
+		wst.write(row, col + 7, arranyar[i]['[']) 
+		wst.write(row, col + 8, arranyar[i][']']) 
+		wst.write(row, col + 9, arranyar[i]['singlequote']) 
+		wst.write(row, col + 10, arranyar[i]['doublequote']) 
+		wst.write(row, col + 11, arranyar[i]['json']) 
+		wst.write(row, col + 12, arranyar[i]['class']) 
+		wst.write(row, col + 13, arranyar[i]['return']) 
+		wst.write(row, col + 14, arranyar[i]['def']) 
+		wst.write(row, col + 15, arranyar[i]['import']) 
+
+		global row
+		row+=1
+
+	wb.close() 
+
+def javaexcel():
+	print 'iki java'
 if __name__ == "__main__":
 	c_file()
 	proc()
